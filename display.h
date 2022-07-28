c487eaa2a8c6fdb517444889aceab77444aadf75
AZ9tumas <77965895+AZ9tumas@users.noreply.github.com>
2022-07-28 13:58:50 +0530

    Update display.h

--- display.h
+++ display.h
@@ -1,17 +1,30 @@
 #ifndef gbc_display_h
 #define gbc_display_h
 
-#define DISPLAY_SCALING 4
+#define DISPLAY_SCALING 5.5
 #define HEIGHT_PX 144
 #define WIDTH_PX  160
 
 #define FIFO_MAX_COUNT 8
 
-#define T_CYCLES_PER_FRAME 70224
-#define T_CYCLES_PER_SCANLINE 456
-#define DEFAULT_FRAMERATE 59.7275
+#define CYCLES_PER_FRAME 70224
+#define CYCLES_PER_SCANLINE 456
+#define DEFAULT_FRAMERATE 63.7164
 
-#define T_CYCLES_PER_MODE2 80
-#define T_CYCLES_PER_VBLANK 4560
+#define CYCLES_PER_MODE2 80
+#define CYCLES_PER_VBLANK 4560
+
+
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_scancode.h>
+#include <SDL2/SDL_video.h>
+
+#include "emulator.h"
+
+void SDL_events(Emulator* emulator);
+void Sync_Display(Emulator* emulator, unsigned long cycles);
+int initSDL(Emulator* emulator);
 
 #endif
